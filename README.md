e961c1bf8cbab97d8509d97b346f163b49682ec5
fenris <fenristhefox@gmail.com>
2022-11-16 00:26:46 -0600

    added documentation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # findAlpha
+# a program to rationalize decimals
+# uses the [Mediant](https://en.wikipedia.org/wiki/Mediant_(mathematics)) method
--- main.cpp
+++ main.cpp
@@ -5,25 +5,28 @@
 
 int main() {
 
+    //the float we try to rationalize
     float alpha = 0.75555555555;
 
-    float upperA = 1;
-    float upperB = 1;
+    //starting parameters, requires 0 < alpha < 1
+    float upperA = 1, upperB = 1, lowerA = 0, lowerB = 1;
 
-    float lowerA = 0;
-    float lowerB = 1;
-
+    //check if we have found a rationalization for alpha
     bool finished = false;
-
     while (!finished) {
+
+        //alpha is between lower limit and the upper limit numerator + the lower limit numerator all over the upper limit denominator + the lower limit denominator
         if (alpha >= lowerA/lowerB && alpha <= (upperA+lowerA)/(upperB+lowerB)) {
             upperA += lowerA;
             upperB += lowerB;
+
+        //alpha is between the upper limit and the upper limit numerator + the lower limit numerator all over the upper limit denominator + the lower limit denominator
         } else if (alpha >= (upperA+lowerA)/(upperB+lowerB) && alpha <= upperA/upperB) {
             lowerA += upperA;
             lowerB += upperB;
         }
 
+        //check if we have found alpha
         if (alpha == (upperA/upperB)) {
             std::cout << "Alpha was rationalized to " << upperA << "/" << upperB;
             finished = true;
